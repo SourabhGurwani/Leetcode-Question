db18310c1c4c1b891f518c81729e3be916758285
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-02 23:56:29 +0530

    Time: 0 ms (100%), Space: 14.8 MB (44.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-         map<int, int> mergedMap;
+        vector<vector<int>> result;
+    int i = 0, j = 0;
 
-    for (auto& pair : nums1) {
-        mergedMap[pair[0]] += pair[1];
+
+    while (i < nums1.size() && j < nums2.size()) {
+        if (nums1[i][0] == nums2[j][0]) {
+            result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+            i++;
+            j++;
+        } else if (nums1[i][0] < nums2[j][0]) {
+            result.push_back(nums1[i]);
+            i++;
+        } else {
+            result.push_back(nums2[j]);
+            j++;
+        }
     }
-    for (auto& pair : nums2) {
-        mergedMap[pair[0]] += pair[1];
+    while (i < nums1.size()) {
+        result.push_back(nums1[i]);
+        i++;
     }
-    vector<vector<int>> result;
-    for (auto& entry : mergedMap) {
-        result.push_back({entry.first, entry.second});
+    while (j < nums2.size()) {
+        result.push_back(nums2[j]);
+        j++;
     }
 
     return result;
