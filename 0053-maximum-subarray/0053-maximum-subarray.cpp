bae093c50211516d90247f5fac5602b044229923
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-04 15:20:46 +0530

    Time: 0 ms (100%), Space: 71.8 MB (18.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        int maxi=INT_MIN;
         int sum=0;
-        //Kadane's Algo
+        int maxi=INT_MIN;
         for(int i=0;i<n;i++){
             sum+=nums[i];
             maxi=max(maxi,sum);
@@ -13,6 +12,7 @@ public:
             }
         }
         return maxi;
+
         
     }
 };
