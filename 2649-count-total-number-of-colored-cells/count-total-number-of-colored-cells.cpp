b3ac3a0aec155ecb96fe52558837ee71fd3b1405
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-05 14:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        // long long sum=0;
+        // if(n==0){
+        //     return 0;
+        // }
+        // for(int i=0;i<1;i++){
+            return 1LL*n*n+1LL*(n-1)*(n-1);
+        // }
+        // return sum;
+        
+    }
+};
