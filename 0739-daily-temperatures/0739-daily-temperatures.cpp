92662e5c458183f478d25d5e6c31b88aced8364f
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-07 20:12:36 +0530

    Time: 26 ms (46.84%), Space: 102.8 MB (80.05%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,20 +4,15 @@ public:
         int n=temp.size();
         vector<int>ans(n,0);
         stack<int>st;
-        int c=0;
         for(int i=n-1;i>=0;i--){
             while(!st.empty() && temp[i]>=temp[st.top()]){
                 st.pop();
-                c++;
             }
             if(!st.empty()){
                 ans[i]=st.top()-i;
             }
             st.push(i);
-            c=0;
         }
-        return ans;
-
-        
+        return ans;   
     }
 };
