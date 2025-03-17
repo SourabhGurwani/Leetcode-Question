8e39cc347893fee6a95543d357c3f65a11d7dd52
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-17 09:26:24 +0530

    Time: 4 ms (42.39%) | Memory: 16.6 MB (92.45%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        bool result=true;
+        for(int i=0;i<nums.size();i=i+2){
+            if(nums[i]!=nums[i+1]){
+                result=false;
+            }
+        }
+        return result;
+    }
+};
