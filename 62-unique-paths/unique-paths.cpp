511f153291ebb4508559851db55bc2daf0dd378e
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-03-30 00:29:05 +0530

    Time: 2 ms (18.70%) | Memory: 9.2 MB (67.24%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //  int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));  // Initialize all cells with 1
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // Apply transition formula
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+    // }
+};
