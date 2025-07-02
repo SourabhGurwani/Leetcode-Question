7520051ac9eab3b06fac708d03cb5499a7290431
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-07-03 05:20:59 +0530

    Time: 2 ms (41.34%) | Memory: 9.7 MB (94.95%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void permute(vector<int> nums,vector<vector<int>> &current, vector<int> &ans,int index){
-       
-        if(index==nums.size()){ 
-        current.push_back(ans);
-        return;
+    void helper(vector<int>& nums,int index,vector<int>& subset,vector<vector<int>>& result){
+        if(index==nums.size()){
+            result.push_back(subset);
+            return;
         }
-        ans.push_back(nums[index]);
-        permute(nums,current,ans,index+1);
-        ans.pop_back();
-        permute(nums,current,ans,index+1);
-        return ;
-    }
+        subset.push_back(nums[index]);
+        helper(nums,index+1,subset,result);
+        subset.pop_back();
+        helper(nums,index+1,subset,result);
+        
 
+
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> current;
-        vector<int> ans;
-        permute(nums,current,ans,0);
-        return current;
+        vector<vector<int>> result;
+        vector<int> subset;
+        helper(nums,0,subset,result);
+        return result;
+        
     }
 };
