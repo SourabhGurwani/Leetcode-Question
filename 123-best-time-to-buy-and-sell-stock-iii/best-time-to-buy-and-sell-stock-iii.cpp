7f7626c1f2d91de24c18d7f4336d3f37b1b3db97
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-07-07 11:23:54 +0530

    Time: 15 ms (81.11%) | Memory: 85.5 MB (73.92%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy1 = INT_MIN, sell1 = 0;
-        int buy2 = INT_MIN, sell2 = 0;
-
-        for (int price : prices) {
-            buy1  = max(buy1, -price);
-            sell1 = max(sell1, buy1 + price);
-            buy2  = max(buy2, sell1 - price);
-            sell2 = max(sell2, buy2 + price);
+        vector<int> leftToRight(prices.size(),0);
+        int minLeft=prices[0];
+        int buy=0;
+        for(int i=0;i<prices.size();i++){
+            
+            buy=max(buy,prices[i]-minLeft);
+            leftToRight[i]=buy;
+            minLeft = min(minLeft, prices[i]);
+            
         }
+        vector<int> rightToLeft(prices.size(),0);
+        int minRight=prices[prices.size()-1];
+        int sell=0;
+        for(int i=prices.size()-1;i>=0;i--){
+            sell=max(sell,minRight-prices[i]);
+            rightToLeft[i]=sell;
+            minRight = max(minRight, prices[i]);
+        }
+        int result=0;
+        for(int i=0;i<prices.size();i++){
+            result=max(result,leftToRight[i]+rightToLeft[i]);
 
-        return sell2;
+        }
+        return result;
+
+        
     }
 };
