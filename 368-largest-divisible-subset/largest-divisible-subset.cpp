efc1df0caaeaa4cb78cdf8bc18b1de6c4c1e0491
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-07-09 14:05:43 +0530

    Time: 10 ms (71.68%) | Memory: 12.6 MB (48.97%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,36 +1,34 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         if (nums.empty()) return {};
+
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> dp(n, 1); // dp[i] = length of largest subset ending at nums[i]
-        vector<int> parent(n, -1); // parent[i] = index of previous element in the subset
-        int max_len = 1, max_idx = 0;
-        
+
+        vector<int> dp(n, 1);      // dp[i] = size of largest subset ending at i
+        vector<int> prev(n, -1);   // prev[i] = previous index in the subset chain
+        int maxIndex = 0;
+
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
                     dp[i] = dp[j] + 1;
-                    parent[i] = j;
+                    prev[i] = j;
                 }
             }
-            if (dp[i] > max_len) {
-                max_len = dp[i];
-                max_idx = i;
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
             }
         }
-        
+
+        // Reconstruct subset
         vector<int> result;
-        while (max_idx != -1) {
-            result.push_back(nums[max_idx]);
-            max_idx = parent[max_idx];
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            result.push_back(nums[i]);
+            if (prev[i] == -1) break;
         }
+
         reverse(result.begin(), result.end());
         return result;
     }
