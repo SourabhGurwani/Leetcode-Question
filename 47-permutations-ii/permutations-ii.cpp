22179fc3b0c986d25c4332e73813f4bc1d22abfd
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-07-13 05:00:32 +0530

    Time: 3 ms (68.18%) | Memory: 11.5 MB (82.23%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,30 +1,33 @@
 class Solution {
-//     struct VectorHash {
-//     size_t operator()(const vector<int>& vec) const {
-//         size_t hashValue = vec.size();
-//         for (int num : vec) {
-//             hashValue ^= hash<int>{}(num) + 0x9e3779b9 + (hashValue << 6) + (hashValue >> 2);
-//         }
-//         return hashValue;
-//     }
-// };
 public:
-    void permute(vector<int> &nums,set <vector<int>> &curr,int index){
-        if(index==nums.size()){
-            curr.insert(nums);
-
+//so in permutation we have to use used array to check the particular element is used or not
+    void fun(vector<int>& nums,vector<vector<int>>& result,vector<bool>& used,vector<int> & subarr){
+        int n=nums.size();
+        
+        if(subarr.size()==nums.size()){
+            result.push_back(subarr);
+            return;
         }
-        for(int i=index;i<nums.size();i++){
-            swap(nums[i],nums[index]);
-            permute(nums,curr,index+1);
-            swap(nums[i],nums[index]);
-
+        for(int i=0;i<n;i++){
+            if(used[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]){
+               continue;
+            
+            }
+                used[i]=true;
+                subarr.push_back(nums[i]);
+                fun(nums,result,used,subarr);
+                subarr.pop_back();
+                used[i]=false;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set <vector<int>> curr;
-        permute(nums,curr,0);
-        return vector<vector<int>> (curr.begin(),curr.end());
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        vector<int> subarr;
+        vector<bool> used(nums.size(),false);
+        fun(nums,result,used,subarr);
+        return result;
         
     }
 };
