3a2d62eb4fa862d1cb2288ecc5ba2ea263eef6eb
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2025-10-23 01:25:42 +0530

    Time: 0 ms (100%), Space: 9.3 MB (60.02%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int uniqueRaste(int row , int col , int m , int n,vector<vector<int>>&dp ){
+        if(row>=m || col>=n)return 0;
+        if(row == m-1 && col ==n-1)return 1;
+        if(dp[row][col]!=-1)return dp[row][col];
+        int right = uniqueRaste(row , col+1 , m ,n ,dp);
+        int down = uniqueRaste(row+1 , col , m ,n ,dp);
+
+        return dp[row][col] = right + down;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        
+        return uniqueRaste(0 , 0 , m , n , dp);
+        
+    }
+};
